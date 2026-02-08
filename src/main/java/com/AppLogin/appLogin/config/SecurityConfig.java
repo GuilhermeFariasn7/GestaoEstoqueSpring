15808693d77d0ec080ebeebfc87186f4061f4898
GuilhermeFariasn7 <guilherme.nazario765@gmail.com>
2026-02-08 15:25:14 -0300

    subindo todo o projeto novamente para aprender a como colocar em host e não mais local a aplicação.

--- src/main/java/com/AppLogin/appLogin/config/SecurityConfig.java
+++ src/main/java/com/AppLogin/appLogin/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(org.springframework.security.config.annotation.web.builders.HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // ✅ Forma correta no 6.1+
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/**").permitAll()
                 );
